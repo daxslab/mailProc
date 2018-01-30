fd6f13417e44afb8b2123b5fcd18ee721132bae0
Carlos Cesar Caballero Díaz <ccesar@daxslab.com>
2018-01-30 12:49:04 -0500

    generate docs on RTD with default RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,8 @@ import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.dirname(__file__))
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -87,7 +89,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+if not on_rtd:
+    html_theme = 'alabaster'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
